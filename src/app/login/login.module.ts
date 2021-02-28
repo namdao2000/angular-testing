57a26ffae4eb21124d7e6e044dea2728166c8d7c
Nam <namdao2000@gmail.com>
2021-02-28 21:18:22 +1100

    removed router stuff from login modules

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,14 +1,6 @@
 import {LoginComponent} from "./login.component";
 import {NgModule} from "@angular/core";
 import {ReactiveFormsModule} from "@angular/forms";
-import {RouterModule, Routes} from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: "",
-    component: LoginComponent
-  }
-];
 
 @NgModule({
   declarations: [
